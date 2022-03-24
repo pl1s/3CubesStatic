31fb49f69a48d568b392a258085253dc7030411b
Shumeras <s.mackonis@gmail.com>
2022-03-24 14:53:02 +0200

    * Fixed some issues with locale.js script     (NOTE: localization language is now indicated by loc_lang in cookie); * Changed popup appearance; * Changed popup to only open on subscribe button presses;

--- res/js/locale.js
+++ res/js/locale.js
@@ -1,27 +1,53 @@
 const langSelector = document.querySelectorAll("#langSelector");
 const logo = document.querySelector(".logoImg");
-let cookie = "";
+var localizationLanguage
 
-//get ONLY lang Cookie
-function getLangCookie() {
-  let cookies = {};
-  let lang = navigator.language || navigator.userLanguage;
-  document.cookie.split(";").map(function (el) {
-    let [k, v] = el.split("=");
-    if (el.length == 2) {
-      cookie = el;
+// Parameter hiding to the next level - EVERYTHING is name cookie...
+// //get ONLY lang Cookie
+// function getLangCookie() {
+//   let cookies = {};
+//   let lang = navigator.language || navigator.userLanguage;
+//   document.cookie.split(";").map(function (el) {
+//     let [k, v] = el.split("=");
+//     if (el.length == 2) {
+//       cookie = el;
+//     }
+//     //language change on default browser lang
+//     if (!cookie && lang === "lt") {
+//       cookie = "lt";
+//     }
+//   });
+//   return cookie;
+// }
+
+// Get localization language from current memory or load it from cookie;
+function getLocalizationLanguage()
+{
+  if(!localizationLanguage)
+  {
+    let localizationParameter = document.cookie.split(";").find(s => s.includes("loc_lang"));
+    if(!localizationParameter)
+    {
+      setLocalizationLanguage("en");
+      localizationParameter="loc_lang=en";
+      return "en";
     }
-    //language change on default browser lang
-    if (!cookie && lang === "lt") {
-      cookie = "lt";
-    }
-  });
-  return cookie;
+    localizationLanguage = localizationParameter.split('=')[1];
+  }  
+
+  return localizationLanguage; 
+}
+
+// Set the localization language parameter in the cookie to the given value
+function setLocalizationLanguage(value, expiration=2592000)
+{
+  document.cookie = `loc_lang=${value}; max-age=${expiration}; path=/;`;
+  localizationLanguage = value;
 }
 
 //toggle logo LT->EN
-function toggleLogo(cookie) {
-  cookie === "en"
+function toggleLogo(lang) {
+  lang === "en"
     ? (logo.src = "/res/img/header_footer/logo_en.png")
     : (logo.src = "/res/img/header_footer/logo_en.png");
 }
@@ -48,7 +74,7 @@ async function i18Loader() {
     return acc;
   }, {});
   await i18next.init({
-    lng: cookie ? cookie : "en",
+    lng: localizationLanguage ? localizationLanguage : "en",
     debug: true,
     resources: res,
   });
@@ -61,23 +87,22 @@ async function i18Loader() {
   langSelector.forEach((s) => {
     s.addEventListener("click", (e) => {
       i18next.changeLanguage(e.target.innerText.toLowerCase());
-      //30days
-      let exp = 2592000;
-      document.cookie = `${e.target.innerText.toLowerCase()}; max-age=${exp};`;
+      setLocalizationLanguage(e.target.innerText.toLowerCase()); // NOTE: Not a very good practice to use visual elements as values; 
       // active class Switcher
-      e.target.innerText.toLowerCase() === "en"
+      localizationLanguage === "en"
         ? s.classList.add("active")
         : langSelector[1].classList.remove("active");
 
-      e.target.innerText.toLowerCase() === "lt"
+      localizationLanguage === "lt"
         ? s.classList.add("active")
         : langSelector[0].classList.remove("active");
 
-      toggleLogo(document.cookie);
+      toggleLogo(localizationLanguage);
     });
   });
   toggleClass();
   toggleLogo();
 }
-getLangCookie();
+
+getLocalizationLanguage();
 i18Loader();
